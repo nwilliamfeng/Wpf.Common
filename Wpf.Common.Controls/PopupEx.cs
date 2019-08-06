2f8a2c6146e266f439b7659a0dcc7d9a1c85d5b9
nwilliamfeng <nwilliamfeng@163.com>
2019-08-07 00:19:23 +0800

    提交些修改

---
+++ Wpf.Common.Controls/PopupEx.cs
@@ -0,0 +1,254 @@
+﻿
+using ControlzEx.Native;
+using ControlzEx.Standard;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+
+namespace Wpf.Common.Controls
+{
+    public class CustomValidationPopup : Popup
+    {
+        private Window hostWindow;
+
+        public static readonly DependencyProperty CloseOnMouseLeftButtonDownProperty
+            = DependencyProperty.Register(nameof(CloseOnMouseLeftButtonDown),
+                                          typeof(bool),
+                                          typeof(CustomValidationPopup),
+                                          new PropertyMetadata(true));
+
+        /// <summary>
+        /// Gets or sets whether if the popup can be closed by left mouse button down.
+        /// </summary>
+        public bool CloseOnMouseLeftButtonDown
+        {
+            get { return (bool)this.GetValue(CloseOnMouseLeftButtonDownProperty); }
+            set { this.SetValue(CloseOnMouseLeftButtonDownProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowValidationErrorOnMouseOverProperty
+            = DependencyProperty.RegisterAttached(nameof(ShowValidationErrorOnMouseOver),
+                                                  typeof(bool),
+                                                  typeof(CustomValidationPopup),
+                                                  new PropertyMetadata(false));
+
+        /// <summary>
+        /// Gets or sets whether the validation error text will be shown when hovering the validation triangle.
+        /// </summary>
+        public bool ShowValidationErrorOnMouseOver
+        {
+            get { return (bool)this.GetValue(ShowValidationErrorOnMouseOverProperty); }
+            set { this.SetValue(ShowValidationErrorOnMouseOverProperty, value); }
+        }
+
+        public CustomValidationPopup()
+        {
+            this.Loaded += this.CustomValidationPopup_Loaded;
+            this.Opened += this.CustomValidationPopup_Opened;
+            this.Closed += CustomValidationPopup_Closed;
+        }
+
+        private async void CustomValidationPopup_Closed(object sender, EventArgs e)
+        {
+          ??  if(_unsafeClose)
+            {
+                _unsafeClose = false;
+                await System.Threading.Tasks.Task.Run(() =>
+                {
+                    System.Threading.Thread.Sleep(100);
+                });
+                this.IsOpen = true;
+            }
+             
+            Console.WriteLine("closed "  );
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            if (this.CloseOnMouseLeftButtonDown)
+            {
+                this.SetCurrentValue(IsOpenProperty, false);
+            }
+            else
+            {
+                var adornedElement = this.GetAdornedElement();
+                if (adornedElement != null )
+                {
+                    this.SetCurrentValue(IsOpenProperty, false);
+                }
+                else
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void CustomValidationPopup_Loaded(object sender, RoutedEventArgs e)
+        {
+            var target = this.PlacementTarget as FrameworkElement;
+            if (target == null)
+            {
+                return;
+            }
+
+            this.hostWindow = Window.GetWindow(target);
+            if (this.hostWindow == null)
+            {
+                return;
+            }
+
+            this.hostWindow.LocationChanged -= this.hostWindow_SizeOrLocationChanged;
+            this.hostWindow.LocationChanged += this.hostWindow_SizeOrLocationChanged;
+            this.hostWindow.SizeChanged -= this.hostWindow_SizeOrLocationChanged;
+            this.hostWindow.SizeChanged += this.hostWindow_SizeOrLocationChanged;
+            target.SizeChanged -= this.hostWindow_SizeOrLocationChanged;
+            target.SizeChanged += this.hostWindow_SizeOrLocationChanged;
+            this.hostWindow.StateChanged -= this.hostWindow_StateChanged;
+            this.hostWindow.StateChanged += this.hostWindow_StateChanged;
+            this.hostWindow.Activated -= this.hostWindow_Activated;
+            this.hostWindow.Activated += this.hostWindow_Activated;
+            this.hostWindow.Deactivated -= this.hostWindow_Deactivated;
+            this.hostWindow.Deactivated += this.hostWindow_Deactivated;
+
+            this.Unloaded -= this.CustomValidationPopup_Unloaded;
+            this.Unloaded += this.CustomValidationPopup_Unloaded;
+        }
+
+        private void CustomValidationPopup_Opened(object sender, EventArgs e)
+        {
+            
+            this.SetTopmostState(true);
+            Console.WriteLine("do open "+this.IsOpen);
+        }
+
+        private void hostWindow_Activated(object sender, EventArgs e)
+        {
+            this.SetTopmostState(true);
+        }
+
+        private void hostWindow_Deactivated(object sender, EventArgs e)
+        {
+            this.SetTopmostState(false);
+        }
+
+        private void CustomValidationPopup_Unloaded(object sender, RoutedEventArgs e)
+        {
+            var target = this.PlacementTarget as FrameworkElement;
+            if (target != null)
+            {
+                target.SizeChanged -= this.hostWindow_SizeOrLocationChanged;
+            }
+
+            if (this.hostWindow != null)
+            {
+                this.hostWindow.LocationChanged -= this.hostWindow_SizeOrLocationChanged;
+                this.hostWindow.SizeChanged -= this.hostWindow_SizeOrLocationChanged;
+                this.hostWindow.StateChanged -= this.hostWindow_StateChanged;
+                this.hostWindow.Activated -= this.hostWindow_Activated;
+                this.hostWindow.Deactivated -= this.hostWindow_Deactivated;
+            }
+
+            this.Unloaded -= this.CustomValidationPopup_Unloaded;
+            this.Opened -= this.CustomValidationPopup_Opened;
+            this.hostWindow = null;
+        }
+
+        private UIElement GetAdornedElement()
+        {
+            var placeholder = this.PlacementTarget is FrameworkElement target ? target.DataContext as AdornedElementPlaceholder : null;
+            return placeholder?.AdornedElement;
+        }
+
+        private void hostWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.hostWindow != null && this.hostWindow.WindowState != WindowState.Minimized)
+            {
+                var adornedElement = this.GetAdornedElement();
+                if (adornedElement != null)
+                {
+                    this.PopupAnimation = PopupAnimation.None;
+                    this.IsOpen = false;
+                    var errorTemplate = adornedElement.GetValue(Validation.ErrorTemplateProperty);
+                    adornedElement.SetValue(Validation.ErrorTemplateProperty, null);
+                    adornedElement.SetValue(Validation.ErrorTemplateProperty, errorTemplate);
+                }
+            }
+        }
+
+        private void hostWindow_SizeOrLocationChanged(object sender, EventArgs e)
+        {
+            var offset = this.HorizontalOffset;
+            // "bump" the offset to cause the popup to reposition itself on its own
+            this.HorizontalOffset = offset + 1;
+            this.HorizontalOffset = offset;
+        }
+
+        private bool? appliedTopMost;
+
+        private bool _unsafeClose;
+        
+        private void SetTopmostState(bool isTop)
+        {
+            // Don’t apply state if it’s the same as incoming state
+            if (this.appliedTopMost.HasValue && this.appliedTopMost == isTop)
+            {
+                Console.WriteLine("aaa");
+                _unsafeClose = true;
+                return;
+            }
+            else
+                Console.WriteLine("bbb"+this.appliedTopMost+"  "+isTop);
+
+            if (this.Child == null)
+            {
+                return;
+            }
+
+            var hwndSource = (PresentationSource.FromVisual(this.Child)) as HwndSource;
+
+            if (hwndSource == null)
+            {
+                return;
+            }
+
+            var hwnd = hwndSource.Handle;
+
+#pragma warning disable 618
+            RECT rect;
+            if (!UnsafeNativeMethods.GetWindowRect(hwnd, out rect))
+            {
+                return;
+            }
+            //Debug.WriteLine("setting z-order " + isTop);
+
+            var left = rect.Left;
+            var top = rect.Top;
+            var width = rect.Width;
+            var height = rect.Height;
+            if (isTop)
+            {
+                NativeMethods.SetWindowPos(hwnd, Constants.HWND_TOPMOST, left, top, width, height, SWP.TOPMOST);
+            }
+            else
+            {
+                // Z-Order would only get refreshed/reflected if clicking the
+                // the titlebar (as opposed to other parts of the external
+                // window) unless I first set the popup to HWND_BOTTOM
+                // then HWND_TOP before HWND_NOTOPMOST
+                NativeMethods.SetWindowPos(hwnd, Constants.HWND_BOTTOM, left, top, width, height, SWP.TOPMOST);
+                NativeMethods.SetWindowPos(hwnd, Constants.HWND_TOP, left, top, width, height, SWP.TOPMOST);
+                NativeMethods.SetWindowPos(hwnd, Constants.HWND_NOTOPMOST, left, top, width, height, SWP.TOPMOST);
+            }
+
+            this.appliedTopMost = isTop;
+#pragma warning restore 618
+        }
+    }
+
+     
+    
+}
