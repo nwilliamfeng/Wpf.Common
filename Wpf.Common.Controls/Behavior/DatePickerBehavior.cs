a0cf0bad4a10f6a5bfdf3a0d61c4298df8169a20
nwilliamfeng <nwilliamfeng@163.com>
2019-08-11 00:22:26 +0800

    添加DatePicker模板

---
+++ Wpf.Common.Controls/Behavior/DatePickerBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Wpf.Common.Controls.Behavior
+{
+    /// <summary>
+    /// 日期框行为，支持水印和圆角
+    /// </summary>
+    public  class DatePickerBehavior 
+    {
+        
+     
+       
+
+        /// <summary>
+        /// 设置圆角
+        /// </summary>
+        public static readonly DependencyProperty CornerRadiusProperty =
+           DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(DatePickerBehavior), new PropertyMetadata(default(CornerRadius),OnCornerRaduisPropertyChanged));
+
+
+        public static void SetCornerRadius(UIElement obj, CornerRadius value) => obj.SetValue(CornerRadiusProperty, value);
+
+        public static CornerRadius GetCornerRadius(UIElement obj) => obj.GetValue<CornerRadius>(CornerRadiusProperty);
+
+ 
+      
+
+        private static void OnCornerRaduisPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs arg)
+        {
+            source.InitializeControlBorderCornerRadius(arg);
+        }
+
+
+
+      
+    }
+}
---
+++ Wpf.Common.Demo/Controls/DatePickerView.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.Common.Demo.Controls
+{
+    /// <summary>
+    /// DateTimePickerView.xaml 的交互逻辑
+    /// </summary>
+    public partial class DatePickerView : UserControl
+    {
+        public DatePickerView()
+        {
+            InitializeComponent();
+            this.DataContext = new DatePickerViewModel();
+        }
+    }
+
+    public class DatePickerViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+
+        private DateTime _date=DateTime.MinValue;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == nameof(Date) && Date <DateTime.Now)
+                    return "请选择日期";
+
+                return null;
+            }
+        }
+
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                this._date = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Date)));
+            }
+
+        }
+
+        public string Error => null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
