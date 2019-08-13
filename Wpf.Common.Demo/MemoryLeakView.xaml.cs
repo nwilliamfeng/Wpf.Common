b046b6f3df1cfca11b84395ef084dca61b9f3837
nwilliamfeng <nwilliamfeng@163.com>
2019-08-13 18:30:17 +0800

    提交些修改

---
+++ Wpf.Common.Demo/MemoryLeakView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.Common.Demo
+{
+    /// <summary>
+    /// MemoryLeakView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MemoryLeakView : UserControl
+    {
+        public MemoryLeakView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for(var i=0;i<100000;i++)
+            this.textBox.TextChanged += (s,arg)=>
+            {
+                Console.WriteLine(this.textBox.Text);
+            };
+             
+        }
+    }
+}
