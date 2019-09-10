15dd88308d4d6e45c173dec1c3b1e5857caef22f
nwilliamfeng@163.com <nwilliamfeng@163.com>
2019-09-10 23:27:15 +0800

    添加asynccommand

---
+++ Wpf.Common.Core/Input/Async/AsyncCommandBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wpf.Common.Input
+{
+    /// <summary>
+    /// https://msdn.microsoft.com/en-us/magazine/dn630647.aspx
+    /// </summary>
+    public abstract class AsyncCommandBase : IAsyncCommand
+    {
+        public abstract bool CanExecute(object parameter);
+
+        public abstract Task ExecuteAsync(object parameter);
+
+        public async void Execute(object parameter)
+        {
+            await ExecuteAsync(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        protected void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+
+}
---
+++ Wpf.Common.Core/Input/Async/IAsyncCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wpf.Common.Input
+{
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync(object parameter);
+    }
+
+}
