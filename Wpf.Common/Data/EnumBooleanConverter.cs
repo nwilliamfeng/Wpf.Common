de95d5e08d385acbec76e972f628eff99a883c81
nwilliamfeng@163.com <nwilliamfeng@163.com>
2019-09-13 21:27:16 +0800

    提交一些converter

---
+++ Wpf.Common/Data/EnumBooleanConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wpf.Common.Data
+{
+    
+    public class EnumBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string parameterString = parameter as string;
+            if (parameterString == null)
+                return DependencyProperty.UnsetValue;
+
+            if (Enum.IsDefined(value.GetType(), value) == false)
+                return DependencyProperty.UnsetValue;
+        
+            object parameterValue = Enum.Parse(value.GetType(), parameterString);
+
+            return parameterValue.Equals(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+            {
+                string parameterString = parameter as string;
+                if (parameterString == null)
+                    return DependencyProperty.UnsetValue;
+
+                return Enum.Parse(targetType, parameterString);
+            }
+            return Binding.DoNothing;
+        }
+        #endregion
+    }
+}
---
+++ Wpf.Common/Data/EqualsConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Windows.Data;
+using System.Diagnostics;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Wpf.Common.Data
+{
+    public class EqualsConverter : IValueConverter
+    {
+        private object defaultValue = Binding.DoNothing;
+        public object DefaultValue
+        {
+            get { return defaultValue; }
+            set { defaultValue = value; }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return object.Equals(value, parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+                return parameter;
+
+            return DefaultValue;
+        }
+    }
+}
