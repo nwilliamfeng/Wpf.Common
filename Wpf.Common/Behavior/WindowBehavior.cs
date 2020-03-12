5eef00103898a95ce1e6e78791865a7473e8af12
nwilliamfeng <nwilliamfeng@163.com>
2020-03-12 11:17:44 +0800

    提交部分修改

---
+++ Wpf.Common/Behavior/WindowBehavior.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Wpf.Common.Behavior
+{
+    /// <summary>
+    ///  WindowBehavior
+    /// </summary>
+    public class WindowBehavior
+    {
+        #region Blink
+        public static readonly DependencyProperty IsBlinkEnableProperty =
+        DependencyProperty.RegisterAttached("IsBlinkEnable", typeof(bool), typeof(WindowBehavior), new PropertyMetadata(false, OnIsBlinkEnablePropertyChanged));
+
+        public static bool GetIsBlinkEnable(DependencyObject obj) => obj.GetValue<bool>(IsBlinkEnableProperty);
+
+        public static void SetIsBlinkEnable(DependencyObject obj, object value) => obj.SetValue(IsBlinkEnableProperty, value);
+
+
+        private static void OnIsBlinkEnablePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var window = sender as Window;
+            if (window == null || !(e.NewValue is bool))
+                return;
+            var isEnable = (bool)e.NewValue;
+            if (isEnable)
+                window.FlashWindow();
+            else
+                window.StopFlashingWindow();
+        }
+
+        public static readonly DependencyProperty CloseBlinkWhenDeactiveProperty =
+       DependencyProperty.RegisterAttached("CloseBlinkWhenDeactive", typeof(bool), typeof(WindowBehavior), new PropertyMetadata(false, OnCloseBlinkWhenDeactiveChange));
+
+        public static bool GetCloseBlinkWhenDeactive(DependencyObject obj) => obj.GetValue<bool>(CloseBlinkWhenDeactiveProperty);
+
+        public static void SetCloseBlinkWhenDeactive(DependencyObject obj, object value) => obj.SetValue(CloseBlinkWhenDeactiveProperty, value);
+
+        private static void OnCloseBlinkWhenDeactiveChange(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var window = sender as Window;
+            if (window == null || !(e.NewValue is bool))
+                return;
+            var isEnable = (bool)e.NewValue;
+            if (!isEnable) return;
+            window.Activated += OnWindowActivated;
+            window.StopFlashingWindow();
+
+        }
+
+        private static void OnWindowActivated(object sender, System.EventArgs e)
+        {
+            (sender as Window).StopFlashingWindow();
+        }
+        #endregion
+
+        #region Esc To Close
+
+        public static readonly DependencyProperty EscToCloseProperty
+            = DependencyProperty.RegisterAttached("EscToClose", typeof(bool), typeof(WindowBehavior), new PropertyMetadata(false, new PropertyChangedCallback(OnEscToClosePropertyChanged)));
+
+        private static void OnEscToClosePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var window = sender as Window;
+            if (window == null) return;
+            if (!(e.NewValue is bool)) return;
+            bool enable = (bool)e.NewValue;
+            if (!enable) return;
+            window.KeyDown -= OnWatchWindow_Esc_KeyDown;
+            window.KeyDown += OnWatchWindow_Esc_KeyDown;
+            window.Unloaded += delegate
+            {
+                window.KeyDown -= OnWatchWindow_Esc_KeyDown;
+            };
+        }
+
+        public static void SetEscToClose(DependencyObject obj, bool enable) => obj.SetValue(EscToCloseProperty, enable);
+
+        public static void GetEscToClose(DependencyObject obj) => obj.GetValue<bool>(EscToCloseProperty);
+
+        private static void OnWatchWindow_Esc_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                (sender as Window)?.Close();
+        }
+
+        #endregion
+    }
+}
