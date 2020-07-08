073ad3ea250374b1159c41b8c2274e1d923a3108
nwilliamfeng <nwilliamfeng@163.com>
2020-07-08 17:40:23 +0800

    添加ReverseBooleanConverter

---
+++ Wpf.Common/Data/ReverseBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wpf.Common.Data
+{
+    public class ReverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                return DependencyProperty.UnsetValue;
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (!(value is bool))
+                return DependencyProperty.UnsetValue;
+            return !(bool)value;
+        }
+
+        #endregion
+    }
+}
